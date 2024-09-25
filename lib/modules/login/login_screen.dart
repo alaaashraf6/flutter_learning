23f170b6cbd62c72e949641551b07a5b20edaf5b
devmoustafa97 <devmoustafa97@gmail.com>
2024-09-25 18:00:25 +0300

    add custom btn component

--- lib/modules/login/login_screen.dart
+++ lib/modules/login/login_screen.dart
@@ -1,13 +1,14 @@
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
+import 'package:learning_app_on_udemy/shared/components/components.dart';
 
 class LoginScreen extends StatelessWidget {
   LoginScreen({super.key});
 
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
-
+var formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,84 +19,110 @@ class LoginScreen extends StatelessWidget {
         padding: const EdgeInsets.all(20),
         child: Center(
           child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(
-                  'Login',
-                  style: TextStyle(
-                    fontSize: 40,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                const SizedBox(
-                  height: 40,
-                ),
-                TextFormField(
-                  controller: emailController,
-                  keyboardType: TextInputType.emailAddress,
-                  onFieldSubmitted: (value) {
-                    // log(value);
-                  },
-                  onChanged: (value) {
-                    // log(value);
-                  },
-                  decoration: const InputDecoration(
-                      labelText: 'Email Address',
-                      border: OutlineInputBorder(),
-                      prefixIcon: Icon(Icons.email)),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                TextFormField(
-                  controller: passwordController,
-                  keyboardType: TextInputType.visiblePassword,
-                  obscureText: true,
-                  onFieldSubmitted: (value) {
-                    // log(value);
-                  },
-                  onChanged: (value) {
-                    // log(value);
-                  },
-                  decoration: const InputDecoration(
-                      labelText: 'Password',
-                      border: OutlineInputBorder(),
-                      prefixIcon: Icon(Icons.lock),
-                      suffixIcon: Icon(Icons.remove_red_eye)),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Container(
-                  width: double.infinity,
-                  color: Colors.blue,
-                  child: MaterialButton(
-                    onPressed: () {
-                      log(emailController.toString());
-                      log(passwordController.toString());
-                    },
-                    child: const Text(
-                      'Login',
-                      style: TextStyle(fontSize: 20, color: Colors.white),
+            child: Form(
+              key: formKey ,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Login',
+                    style: TextStyle(
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Row(
-                  children: [
-                    const Text('don\'t have accout'),
-                    TextButton(
-                        onPressed: () {},
-                        child: const Text(
-                          'Register Now',
-                          style: TextStyle(color: Colors.blue),
-                        ))
-                  ],
-                )
-              ],
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  TextFormField(
+                    controller: emailController,
+                    keyboardType: TextInputType.emailAddress,
+                    onFieldSubmitted: (value) {
+                      // log(value);
+                    },
+                    onChanged: (value) {
+                      // log(value);
+                    },
+                    validator: (value){
+                      if ( value ==  null ||  value.isEmpty) {
+                        return 'email address is requird';
+                        
+                      }
+                      return null;
+                      
+
+                    },
+                    decoration: const InputDecoration(
+                        labelText: 'Email Address',
+                        border: OutlineInputBorder(),
+                        prefixIcon: Icon(Icons.email)),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  TextFormField(
+                    controller: passwordController,
+                    keyboardType: TextInputType.visiblePassword,
+                    obscureText: true,
+                    onFieldSubmitted: (value) {
+                      // log(value);
+                    },
+                    onChanged: (value) {
+                      // log(value);
+                    },
+                    validator: (value){
+                      if (value== null|| value.isEmpty ) {
+                        return 'the password id required';
+                        
+                      }
+                      return null ;
+
+                    },
+                    decoration: const InputDecoration(
+                        labelText: 'Password',
+                        border: OutlineInputBorder(),
+                        prefixIcon: Icon(Icons.lock),
+                        suffixIcon: Icon(Icons.remove_red_eye)),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  defaultButtom(
+                      title: 'Login',
+                      bgColor: Colors.blue,
+                      width: double.infinity,
+                      onPressed: () {
+                        if (formKey.currentState?.validate() ?? false) {
+                        log(emailController.text);
+                          
+                        }
+                      }),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  defaultButtom(
+                      title: 'Register',
+                      bgColor: Colors.blue,
+                      width: double.infinity,
+                      onPressed: () {
+                        log(passwordController.text);
+                      }),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  // Row(
+                  //   children: [
+                  //     const Text('don\'t have accout'),
+                  //     TextButton(
+                  //         onPressed: () {},
+                  //         child: const Text(
+                  //           'Register Now',
+                  //           style: TextStyle(color: Colors.blue),
+                  //         ))
+                  //   ],
+                  // )
+                ],
+              ),
             ),
           ),
         ),
